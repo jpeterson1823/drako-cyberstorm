c7e28f73915267c7fd8c2bd2a27f612958186e5d
John Peterson <petersonjohn418@gmail.com>
2025-04-01 09:51:22 -0500

    Generator's Caesar, Vigenere, and XOR methods fully implemented.

--- hidden_challenge/generator/src/drako/crypto.rs
+++ hidden_challenge/generator/src/drako/crypto.rs
@@ -17,38 +17,31 @@ pub mod caesar {
 
     /// Caesar cipher encryption method
     pub fn encrypt(data: &[u8], table: &[u8], n: u8) -> Result<Vec<u8>, CipherError> {
-        // encrypt each byte
-        let mut ciphertext: Vec<u8> = data.to_vec();
-        for byte in ciphertext.iter_mut() {
-            encrypt_byte(byte, table, n);
+        let mut ciphertext: Vec<u8> = Vec::new();
+        for i in 0..data.len() {
+            let index: usize = table.iter().position(|b| *b == data[i]).unwrap();
+            let shift: usize = n as usize;
+            ciphertext.push(table[(index + shift) % table.len()]);
         }
+
         Ok(ciphertext)
     }
 
     /// Caesar cipher decryption method
     pub fn decrypt(data: &[u8], table: &[u8], n: u8) -> Result<Vec<u8>, CipherError> {
-        let mut plaintext: Vec<u8> = data.to_vec();
-        for byte in plaintext.iter_mut() {
-            decrypt_byte(byte, table, n);
+        let mut plaintext: Vec<u8> = Vec::new();
+        for i in 0..data.len() {
+            let index: usize = table.iter().position(|b| *b == data[i]).unwrap();
+            let shift: usize = n as usize;
+            if index >= shift as usize {
+                plaintext.push(table[index - shift]);
+            } else {
+                plaintext.push(table[(table.len() - (shift - index)) % table.len()]);
+            }
         }
         Ok(plaintext)
     }
 
-    /// Attempts to encrypt a provided byte.
-    /// If the provided byte is not found within the provided table, no encrypting occurs.
-    fn encrypt_byte(byte: &mut u8, table: &[u8], n: u8){
-        for b in table.iter().enumerate() {
-            if byte == b.1 { *byte = table[(b.0 + n as usize) % table.len()]; }
-        }
-    }
-
-    /// Attempts to decrypt a provided byte.
-    /// If the provided byte is not found within in the prodived table, no encrypting occurs.
-    fn decrypt_byte(byte: &mut u8, table: &[u8], n: u8) {
-        for b in table.iter().enumerate() {
-            if byte == b.1 { *byte = table[b.0.abs_diff(n as usize) & table.len()]; }
-        }
-    }
 }
 
 pub mod vigenere {
